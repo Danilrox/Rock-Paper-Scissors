782902197c7471c4786a221c32c79e366d5fe7cd
Danilrox <josedmaldonadomr@gmail.com>
2026-09-05 10:27:39 -0500

    Create playRound function and playGame function
    playRound is useful to validating the winner of the round.
    
    playGame is useful to validating the winner of a 5 rounds game.

--- script.js
+++ script.js
@@ -13,5 +13,78 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-   return prompt("rock, paper or scissors?");
+   return prompt("rock, paper or scissors?").toLowerCase();
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("Tie!")
+                break;
+            
+            case "paper":
+                console.log("You lose!, paper beats rock.");
+                return ++computerScore;
+            
+            case "scissors":
+                console.log("You win!, rock beats scissors");
+                return ++humanScore;
+        }
+    }
+
+    if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You win!, paper beats rock.");
+                return ++humanScore;
+            
+            case "paper":
+                console.log("Tie.");
+                break;
+            
+            case "scissors":
+                console.log("You lose!, scissors beats paper");
+                return ++computerScore;
+        }
+    }
+
+    if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You lose!, rock beats scissors.");
+                return ++computerScore;
+            
+            case "paper":
+                console.log("You win!, scissors beats paper.");
+                return ++humanScore;
+            
+            case "scissors":
+                console.log("Tie.");
+                break;
+        }
+    }
+}
+
+function playGame () {
+    for (let i = 0; i < 5; i++) {
+    
+        playRound(getHumanChoice(), getComputerChoice());
+
+        console.log("Human: " + humanScore);
+        console.log("Computer: " + computerScore);
+    }
+    if (humanScore > computerScore) {
+        console.log("Human win");
+    } else if (humanScore < computerScore) {
+        console.log("Computer win");
+    } else {
+        console.log("Tie");
+    }
+
+}
+
+playGame();
